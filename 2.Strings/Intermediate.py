b3218fdad667da08a27933f21f62cea48131ce22
Alok77it <aloktrivedi.it@gmail.com>
2025-02-24 22:49:47 +0530

    Update

---
+++ 2.Strings/Intermediate.py
@@ -0,0 +1,31 @@
+#Check if "123abc" is alphanumeric and if "Python123" contains only alphabets.
+
+s1 = "123abc"
+
+print(s1.isalnum())
+
+
+#Replace "bad" with "good" in the string "This is a bad example".
+
+s1 = "This is a bad example"
+
+print(s1.replace("bad", "good"))        
+
+#Use .split() to break the sentence "Python is awesome" into words.
+
+s1 = "Python is awesome"
+
+print(s1.split("is"))
+
+
+#Find the index of "Python" in "I love Python programming".
+
+s1 = "I Love Python programming"
+
+print(s1.find("Python"))
+
+#Check if " " is a whitespace-only string.
+
+s1 = "I Love Python programming"
+
+print(s1.isspace())
--- Practice/practice2.py
+++ Practice/practice2.py
@@ -1,1 +1,30 @@
-print("Alok")
+name = "ALok"
+
+nameshort = name[0:2] #start from 0 and go upto 2
+
+print(nameshort)
+
+
+print(name[-4:-1])
+
+s1 = "Alok"
+
+s2 = "Trivedi"
+
+s3 = s1 + " " + s2
+
+print(s3.lower())
+print(s3.upper())
+print(s3.strip())
+print(s3.replace("Alok", "Python"))
+print(s3.split())
+
+s = "hello world"
+
+print(s.capitalize())  # 'Hello world'
+print(s.title())       # 'Hello World'
+print(s.count('l'))    # 3 (Count occurrences of 'l')
+print(s.startswith('hello'))  # True
+print(s.endswith('d'))        # True
+print(s.find('world'))        # 6 (Index where 'world' starts)
+print(s.index('o'))           # 4 (First occurrence of 'o')
