200460dbbb953876931a29f27dd50993ce69ba8f
Alok77it <aloktrivedi.it@gmail.com>
2025-02-27 13:22:44 +0530

    Update

---
+++ 3. Numbers/BaiscPractice.py
@@ -0,0 +1,48 @@
+# Arithmetic Operations
+    #Write a Python script to perform addition, subtraction, multiplication, division, floor division, modulus, and exponentiation on two numbers.
+
+a = int(input("First number:" ))
+b = int(input("Second number: "))
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+#Find Even or Odd
+    #Write a function that takes an integer and returns whether it is even or odd.
+
+num = int(input("Enter the number: " ))
+if (num % 2 == 0 ):
+    print ("even")
+else:
+    print("Odd")
+
+
+#Number Conversion
+    #Convert a given decimal number into binary, octal, and hexadecimal.
+
+
+#Factorial Calculation
+    #Write a function to compute the factorial of a number using a loop.
+
+
+#Round a Float
+    #Take a floating-point number and round it to two decimal places.
+
+
+#Simple Interest Calculation
+    #Write a program to calculate simple interest using the formula:
+'''   SI = P x R x T 
+     ----------------
+           100
+'''        
+
+
+
+
+#Find Maximum and Minimum
+    #Find the maximum and minimum of three numbers using the max() and min() functions.
+
