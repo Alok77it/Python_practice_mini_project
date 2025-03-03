3549c3828a43265dbe0c5c9457504347fc4594c1
Alok77it <aloktrivedi.it@gmail.com>
2025-03-03 15:27:49 +0530

    Update

--- 5. Dictionary & Sets/Dictionary.py
+++ 5. Dictionary & Sets/Dictionary.py
@@ -2,6 +2,14 @@
 
     #Create a dictionary with keys: name, age, and city. Assign values to them and print the dictionary.
 
+my_dict = {
+    "name" : "Alok",
+    "age" : 22,
+    "skills" : ["python", "DevOps"]
+}
+print(my_dict)
+
+
 #2️⃣ Accessing Dictionary Values
 
     #Given the dictionary:
@@ -10,25 +18,53 @@
 
     #Print the values of "name" and "course" using both [] and .get() methods.
 
+student = {"name": "Alok", "age": 22, "course": "DevOps"}
+print(student["name"])
+print(student.get("age"))
+
 #3️⃣ Updating Dictionary Values
 
     #Update the "age" key in the dictionary to 23.
+
+student["age"] = 23
+print(student)
+
 
 #4️⃣ Adding a New Key-Value Pair
 
     #Add a new key "skills" with the value ["Python", "Kubernetes"].
 
+student["skills"] = ["Python", "Kubernetes"]
+print(student)
+
 #5️⃣ Removing Elements
 
     #Remove the key "city" from the dictionary using pop().
+student = {"name": "Alok", "age": 22, "course": "DevOps", "city": "Bangalore"}
+student.pop("city")
+print(student)
 
 #6️⃣ Iterating Over a Dictionary
 
     #Write a program to iterate through a dictionary and print both keys and values.
 
+student = {"name": "Alok", "age": 23, "course": "DevOps", "skills": ["Python", "Kubernetes"]}
+for key, value in student.items():
+    print(f"{key} : {value}")
+
+
 #7️⃣ Check If a Key Exists
 
     #Write a function to check if "salary" exists in a dictionary.
+
+def check_status(dictionary):
+    if "salary" in dictionary:
+        return True
+    else:
+        return False
+
+student = {"name": "Alok", "age": 23, "course": "DevOps", "skills": ["Python", "Kubernetes"]}
+print(check_status(student))
 
 #8️⃣ Merging Two Dictionaries
 
@@ -37,6 +73,10 @@
 #dict1 = {"a": 1, "b": 2}
 #dict2 = {"c": 3, "d": 4}
 
+dic1 = {"a":1, "b":2}
+dic2 = {"c":3, "d":4}
+dic1.update(dic2)
+print(dic1)
 
 #9️⃣ Count Word Frequency
 
@@ -49,11 +89,16 @@
 
     #{"python": 2, "is": 2, "fun": 1, "and": 1, "powerful": 1}
 
+
+
+
 #🔟 Find the Student with the Highest Marks
 
     #Given a dictionary of student names and their marks:
 
 #students = {"Alok": 85, "Ravi": 92, "Priya": 78}
+
+
 
 
 #1️⃣1️⃣ Invert a Dictionary
