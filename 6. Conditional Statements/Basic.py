706bd3a122787c59aef2a58e0efde9298919fd02
Alok <154581800+Alok77it@users.noreply.github.com>
2025-03-05 15:13:46 +0530

    Update Basic.py

--- 6. Conditional Statements/Basic.py
+++ 6. Conditional Statements/Basic.py
@@ -94,4 +94,14 @@ else:
 
 #🔟 Write a program to check whether a given number is prime or not.
 
+if number > 1:
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            print(f"{number} is not a prime number")
+            break
+    else:
+        print(f"{number} is a prime number")
+else:
+    print(f"{number} is not a prime number")
+
         
