ce2edd373fa1f386d6bccdf906791b14726972a4
Alok77it <aloktrivedi.it@gmail.com>
2025-03-26 17:53:10 +0530

    Update

--- 9. Modules/Practice.py
+++ 9. Modules/Practice.py
@@ -1,3 +1,5 @@
+'''Exercises Level 1'''
+
 #1.Write a function which generates a six digit/character random_user_id.
 
 import random
@@ -27,3 +29,52 @@ def rgb_color_gen():
     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 print(rgb_color_gen())
+
+'''Exercises Level 2'''
+
+#Write a function list_of_hexa_colors which returns any number of hexadecimal colors in an array 
+# (six hexadecimal numbers written after #.
+# Hexadecimal numeral system is made out of 16 symbols, 0-9 and first 6 letters of the alphabet, a-f. 
+# Check the task 6 for output examples).
+#Write a function list_of_rgb_colors which returns any number of RGB colors in an array.
+#Write a function generate_colors which can generate any number of hexa or rgb colors.
+
+def list_of_hexa_colors(num_colors):
+    colors = []
+    for _ in range(num_colors):
+        color = '#' + ''.join(random.choices('0123456789abcdef', k=6))
+        colors.append(color)
+    return colors
+
+def list_of_rgb_colors(num_colors):
+    colors = []
+    for _ in range(num_colors):
+        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        colors.append(color)
+    return colors
+def generate_colors(num_colors, color_type='hexa'):
+    if color_type == 'hexa':
+        return list_of_hexa_colors(num_colors)
+    elif color_type == 'rgb':
+        return list_of_rgb_colors(num_colors)
+    else:
+        return 'Invalid color type'
+    
+print(generate_colors(3, 'hexa'))
+print(generate_colors(3, 'rgb'))
+print(generate_colors(3, 'cmyk'))
+
+'''Exercises Level 3'''
+
+# Call your function shuffle_list, it takes a list as a parameter and it returns a shuffled list
+# Write a function which returns an array of seven random numbers in a range of 0-9. 
+# All the numbers must be unique.
+
+def shuffle_list(lst):
+    return random.sample(lst, len(lst))
+
+def random_numbers():
+    numbers = list(range(10))
+    return shuffle_list(numbers)
+
+print(random_numbers())
