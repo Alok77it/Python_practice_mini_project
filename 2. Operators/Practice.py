cb5224a7746f5d374eb5ef1f3c0d919777cb79ba
Alok77it <aloktrivedi.it@gmail.com>
2025-03-28 12:08:17 +0530

    Operators

---
+++ 2. Operators/Practice.py
@@ -0,0 +1,73 @@
+# Declare your age as an integer variable
+# Declare your height as a float variable
+# Declare a variable that stores a complex number
+
+# Write a script that prompts the user to enter base and height of the triangle 
+# and calculate the area of this triangle (area = 0.5 * b * h).
+# Example:
+#     Enter base: 20
+#     Enter height: 10
+#     The area of the triangle is 100
+
+# Write a script that prompts the user to enter side a, side b, and side c of the triangle.
+# Calculate the perimeter of the triangle (perimeter = a + b + c).
+# Example:
+#     Enter side a: 5
+#     Enter side b: 4
+#     Enter side c: 3
+#     The perimeter of the triangle is 12
+
+# Get the length and width of a rectangle using input prompts.
+# Calculate its area (area = length * width) and perimeter (perimeter = 2 * (length + width)).
+
+# Get the radius of a circle using input prompt.
+# Calculate the area (area = pi * r * r) and circumference (c = 2 * pi * r) where pi = 3.14.
+
+# Calculate the slope, x-intercept, and y-intercept of the equation y = 2x - 2.
+
+# Using the slope formula (m = (y2 - y1) / (x2 - x1)), 
+# find the slope and Euclidean distance between points (2,2) and (6,10).
+
+# Compare the slopes from tasks 8 and 9.
+
+# Calculate the value of y using the equation y = x^2 + 6x + 9.
+# Try different x values and determine at what x value y equals 0.
+
+# Find the length of the words 'python' and 'dragon' and make a falsy comparison statement.
+
+# Use the 'and' operator to check if 'on' is found in both 'python' and 'dragon'.
+
+# Check if the word "jargon" is in the sentence: "I hope this course is not full of jargon."
+
+# Check if there is no 'on' in both 'dragon' and 'python'.
+
+# Find the length of the text 'python', convert it to a float, and then convert it to a string.
+
+# Even numbers are divisible by 2 with a remainder of zero.
+# Write a Python statement to check if a number is even.
+
+# Check if the floor division of 7 by 3 is equal to the integer-converted value of 2.7.
+
+# Check if the type of '10' is equal to the type of 10.
+
+# Check if int('9.8') is equal to 10.
+
+# Write a script that prompts the user to enter hours and rate per hour.
+# Calculate the weekly pay of the person.
+# Example:
+#     Enter hours: 40
+#     Enter rate per hour: 28
+#     Your weekly earning is 1120.
+
+# Write a script that prompts the user to enter the number of years they have lived.
+# Calculate the number of seconds they have lived, assuming a person can live up to 100 years.
+# Example:
+#     Enter number of years you have lived: 100
+#     You have lived for 3153600000 seconds.
+
+# Write a Python script that displays the following table:
+# 1 1 1 1 1
+# 2 1 2 4 8
+# 3 1 3 9 27
+# 4 1 4 16 64
+# 5 1 5 25 125
