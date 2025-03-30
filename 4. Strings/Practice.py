23cf0554936c8e6fd74f9231a95c198007f6bd51
Alok77it <aloktrivedi.it@gmail.com>
2025-03-30 11:35:01 +0530

    Strings

---
+++ 4. Strings/Practice.py
@@ -0,0 +1,136 @@
+# Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+# Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+string1 = "Thirty"
+string2 = "Days"
+string3 = "Of"
+string4 = "Python"
+full_string = string1 + " " + string2 + " " + string3 + " " + string4
+print(full_string)
+
+string5 = "Coding"
+string6 = "For"
+string7 = "All"
+full_string2 = string5 + " " + string6 + " " + string7
+print(full_string2)
+
+# Declare a variable named company and assign it to an initial value "Coding For All".
+# Print the variable company using print().
+# Print the length of the company string using len() method and print().
+comapany = full_string2
+print(comapany)
+print(len(comapany))
+
+# Change all the characters to uppercase letters using upper() method.
+# Change all the characters to lowercase letters using lower() method.
+# Use capitalize(), title(), swapcase() methods to format the value of the string "Coding For All".
+string1 = "coding for all"
+string2 = "CODING FOR ALL"
+string3 = "Coding For All"
+print(string1.upper())
+print(string2.lower())
+print(string3.capitalize())
+print(string3.title())
+print(string3.swapcase())
+
+# Cut(slice) out the first word of "Coding For All" string.
+# Check if "Coding For All" string contains a word "Coding" using the method index, find or other methods.
+string = "Coding For All"
+first_word = string.split()[0]
+print(first_word)
+
+# Replace the word "Coding" in the string 'Coding For All' to "Python".
+# Change "Python for Everyone" to "Python for All" using the replace method or other methods.
+string = "Coding For All"
+string = string.replace("Coding", "Python")
+print(string)
+string = "Python for Everyone"
+string = string.replace("Python for Everyone", "Python for All")
+print(string)
+
+# Split the string 'Coding For All' using space as the separator (split()).
+# Split the string "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" at the comma.
+string = "Coding For All"
+words = string.split()
+print(words)
+string = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+words = string.split(", ")
+print(words)
+
+# What is the character at index 0 in the string "Coding For All".
+# What is the last index of the string "Coding For All".
+# What character is at index 10 in "Coding For All" string.
+str = "Coding For All"
+print(str[0])  # First character
+print(str[-1])  # Last character
+print(str[10])  # Character at index 10
+
+# Create an acronym or an abbreviation for the name 'Python For Everyone'.
+# Create an acronym or an abbreviation for the name 'Coding For All'.
+
+
+# Use index to determine the position of the first occurrence of 'C' in "Coding For All".
+# Use index to determine the position of the first occurrence of 'F' in "Coding For All".
+# Use rfind to determine the position of the last occurrence of 'l' in "Coding For All People".
+
+
+# Use index or find to find the position of the first occurrence of the word 'because' in the sentence:
+# 'You cannot end a sentence with because because because is a conjunction'
+
+
+# Use rindex to find the position of the last occurrence of the word 'because' in the same sentence.
+
+
+# Slice out the phrase 'because because because' in the sentence:
+# 'You cannot end a sentence with because because because is a conjunction'
+
+
+# Find the position of the first occurrence of the word 'because' in the sentence:
+# 'You cannot end a sentence with because because because is a conjunction'
+
+
+# Slice out the phrase 'because because because' in the sentence:
+# 'You cannot end a sentence with because because because is a conjunction'
+
+
+# Does "Coding For All" start with a substring "Coding"?
+# Does "Coding For All" end with a substring "coding"?
+
+
+# Remove the left and right trailing spaces in the given string: '   Coding For All      '.
+
+
+# Which one of the following variables return True when we use the method isidentifier():
+# 30DaysOfPython
+# thirty_days_of_python
+
+
+# The following list contains the names of some python libraries:
+# ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. 
+# Join the list with a hash with space string.
+
+
+# Use the new line escape sequence to separate the following sentences:
+# I am enjoying this challenge.
+# I just wonder what is next.
+
+
+# Use a tab escape sequence to write the following lines:
+# Name      Age     Country   City
+# Asabeneh  250     Finland   Helsinki
+
+
+# Use the string formatting method to display the following:
+# radius = 10
+# area = 3.14 * radius ** 2
+# The area of a circle with radius 10 is 314 meters square.
+
+
+# Make the following using string formatting methods:
+# 8 + 6 = 14
+# 8 - 6 = 2
+# 8 * 6 = 48
+# 8 / 6 = 1.33
+# 8 % 6 = 2
+# 8 // 6 = 1
+# 8 ** 6 = 262144
+
