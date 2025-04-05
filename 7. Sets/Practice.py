a1ec9826cc21ef3c3786ca649a39369beb07fb2b
Alok77it <aloktrivedi.it@gmail.com>
2025-04-05 12:20:28 +0530

    Dictionaries

--- 7. Sets/Practice.py
+++ 7. Sets/Practice.py
@@ -34,17 +34,35 @@ print(it_companies)
 # Exercises: Level 2
 
 # 6. Join sets A and B.
+
+
 # 7. Find the intersection of A and B.
+
+
 # 8. Check if A is a subset of B.
+
+
 # 9. Determine if A and B are disjoint sets.
+
+
 # 10. Join A with B and B with A.
+
+
 # 11. Find the symmetric difference between A and B.
+
+
 # 12. Delete the sets completely.
 
 # Exercises: Level 3
 
 # 13. Convert the list of ages to a set and compare the length of the list and the set. Which one is bigger?
+
+
 # 14. Explain the difference between the following data types: string, list, tuple, and set.
+
+
 # 15. Count the number of unique words in the given sentence:
 #     "I am a teacher and I love to inspire and teach people."
 #     - Use the split() method and a set to get the unique words.
+
+
--- 8. Dictionaries/Practice.py
+++ 8. Dictionaries/Practice.py
@@ -1,22 +1,58 @@
 # Create an empty dictionary called dog
+dog = {}
+print(dog)
 
 # Add name, color, breed, legs, and age to the dog dictionary
+dog['name'] = 'Buddy'
+dog['color'] = 'Brown'
+dog['breed'] = 'Labrador'
+dog['legs'] = 4
+dog['age'] = 5
+print(dog)
 
 # Create a student dictionary and add first_name, last_name, gender, age, 
 # marital status, skills, country, city, and address as keys for the dictionary
-
+student = {
+    "first_name" : 'John',
+    "last_name" : 'Doe',
+    "gender" : "Male",
+    "age" : 20,
+    "marital_status" : "Single",
+    "skills" : ["Python", "Java", "C++"],
+    "country" : "USA",
+    "city" : "New York",
+    "address" : "123 Main St"
+}
+print(student)
+    
+    
 # Get the length of the student dictionary
+print(len(student))
 
 # Get the value of skills and check the data type, it should be a list
+print(student['skills'])
+print(type(student['skills']))
 
 # Modify the skills values by adding one or two skills
+student['skills'].append('JavaScript')
+student['skills'].append('HTML')
+print(student['skills'])
 
 # Get the dictionary keys as a list
+print(student.keys())  # Get the dictionary keys as a list
 
 # Get the dictionary values as a list
+print(student.values())  # Get the dictionary values as a list
+
 
 # Change the dictionary to a list of tuples using the items() method
+student_items = student.items()
+print(student_items)
 
 # Delete one of the items in the dictionary
+student.pop('address')
+print(student)
 
 # Delete one of the dictionaries
+student.clear()
+print(student)
