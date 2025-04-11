150dd820764613372797c2b0f442c260aade3ef6
Alok77it <aloktrivedi.it@gmail.com>
2025-04-11 19:18:53 +0530

    Update

---
+++ 12. File Handling/Practice.py
@@ -0,0 +1,29 @@
+# Q1. Write a Python program to create a new file named test.txt and write the string "Hello, Python File I/O!" into it.
+
+
+# Q2. Write a Python program to read the contents of the file test.txt and print them.
+
+
+# Q3. Write a program to append the line "This is an appended line." to the file test.txt.
+
+
+# Q4. Write a Python program to count the number of lines in a file called test.txt.
+
+
+# Q5. Write a Python program to read a file line by line and store each line in a list.
+
+
+# Q6. Write a program to read a file and print only those lines which contain more than 30 characters.
+
+
+# Q7. Write a Python program to find and print the number of words in a file.
+
+
+# Q8. Write a Python program to copy the content of one file (source.txt) to another file (destination.txt).
+
+
+# Q9. Write a Python program to read a file and display its content in reverse order (last line first).
+
+
+# Q10. Write a Python program to open a file, replace all occurrences of the word "Python" with "Java", and save the result in a new file.
+
---
+++ Only for practice/Pracitce.py
@@ -0,0 +1,12 @@
+ with open("demo.txt", "w") as file:
+    file.write("This is new message.\n")
+    
+with open("demo.txt", "a") as file:
+    file.write("new append line")    
+    
+with open()
+
+with open('demo.txt', "r") as file:
+    content = file.read()
+    print(content)
+    
