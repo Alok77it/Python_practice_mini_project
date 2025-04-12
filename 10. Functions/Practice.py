b181ae8d6cc71930f9bd26bcfd793382e75d92a5
Alok77it <aloktrivedi.it@gmail.com>
2025-04-12 17:09:17 +0530

    Functions

--- 10. Functions/Practice.py
+++ 10. Functions/Practice.py
@@ -32,21 +32,43 @@ print(status)
 print(type(status))
 
 # 5. Write a function using *args to calculate the sum of any number of numbers.
+def sum_all(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
 
+all = sum_all(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(all)
 
 # 6. Write a function using **kwargs to print employee details (like name, id, location).
+def employee_details(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+        
+employee_details(name="John Doe", age=30, department="HR", salary=50000)
 
 
 # 7. Create a function with both *args and **kwargs, and print both inside the function.
-
+def mixed_args(*args, **kwargs):
+    print("Positional arguments:", args)
+    print("Keyword arguments:", kwargs)
+mixed_args(1, 2, 3, name="Alice", age=25, city="New York")
 
 # 8. Write a lambda function to square a number.
+square = lambda x: x ** 2
+print(square(5))
 
 
 # 9. Write a lambda function to check if a number is even or odd.
-
+is_even = lambda x: "Even"  if x % 2 == 0 else "Odd"
+print(is_even(4))  # Output: Even
+print(is_even(5))  # Output: Odd 
 
 # 10. Create a list of numbers and use `map()` with a lambda to double each number.
+num = [1, 2, 3, 4, 5]
+double = list(map(lambda x: x * 2, num))
+print(double)  
 
 
 # 11. Write a function that demonstrates polymorphism by accepting two different types of arguments (e.g., string and list) and printing their lengths.
--- Only for practice/Pracitce.py
+++ Only for practice/Pracitce.py
@@ -1,8 +1,3 @@
-def multiple():
-    name = "Alok"
-    age = 22
-    return name, age
-
-status = multiple()
-print(status)
-print(type(status))
+is_even = lambda x: "Even"  if x % 2 == 0 else "Odd"
+print(is_even(4))  # Output: Even
+print(is_even(5))  # Output: Odd 
